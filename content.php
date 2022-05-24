d00e9ac655aa5d68263f50249c9fe2d986d96d20
Abdualiym <a.abdualiym@gmail.com>
2022-05-24 16:01:48 +0500

    list added

--- content.php
+++ content.php
@@ -4,12 +4,24 @@
 
 <div class="container">
 
+    <ul class="list-group">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            A list item
+            <span class="badge bg-primary rounded-pill">14</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            A second list item
+            <span class="badge bg-primary rounded-pill">2</span>
+        </li>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            A third list item
+            <span class="badge bg-primary rounded-pill">1</span>
+        </li>
+    </ul>
+
+
 
     <div class="card-deck mb-3 text-center">
-
-
-
-
         <script>
             $( function() {
                 $( "#accordion" ).accordion();
